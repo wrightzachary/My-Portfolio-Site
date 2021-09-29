cab4bcd180562b26f3c67c8e57fff8bfdd4a4aa4
Zachary <taylorzw96@gmail.com>
2021-09-28 21:35:08 -0500

    Changed H1 text on home to a react-typed div

--- portfolio/src/Components/Home/home.jsx
+++ portfolio/src/Components/Home/home.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './home.css';
 import bitmoji1 from '../Images/bitmoji1.png';
+import Typed from 'react-typed';
 
 const Home = () => {
     return ( 
         <React.Fragment>
             <div id="home" className="home">
-                <h1>Welcome to my portfolio!</h1>
+                <h1>I am a Software<Typed 
+                    strings={[' Developer',
+                    ' Engineer',]}
+                    typeSpeed={60}  
+                    loop
+                /></h1>
                 <img src={bitmoji1} alt="Bitmoji" />
             </div>
         </React.Fragment>
