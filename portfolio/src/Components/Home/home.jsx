5336d2c1be57fa0d23f2a19722a7ba2517a4caa0
Zachary Wright <80498861+wrightzachary@users.noreply.github.com>
2022-01-04 00:31:52 -0600

    Re-configured theHome Component

--- portfolio/src/Components/Home/home.jsx
+++ portfolio/src/Components/Home/home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Col, Container, Row, Card } from "react-bootstrap";
 import './home.css';
 import bitmoji1 from '../Images/bitmoji1.png';
 import Typed from 'react-typed';
@@ -7,13 +8,25 @@ const Home = () => {
     return ( 
         <React.Fragment>
             <div id="home" className="home">
-                {/* <h1>I am a Software <Typed 
-                    strings={[' Developer',
-                    ' Engineer',]}
-                    typeSpeed={60}  
-                    loop
-                /></h1> */}
-                <img src={bitmoji1} alt="Bitmoji" width="25%" />
+            <Container>
+            <Row>
+                <Col sm={8}>
+                    <h1>Howdy, Y'all!</h1>
+                    <h5>My Name is</h5>
+                    <h1>Zachary Wright</h1>
+                    <h2>I build solutions to real world problems</h2>
+                    <h5>
+                        I am a Software Developer that is looking for his first role in the tech industry. 
+                        Currently I am focused on growing my knowledge while I find my first developement 
+                        role, but I am  also a co-host for the podcast codeFellowship().
+                    </h5>
+                </Col>
+                {/* <Col sm={1}></Col> */}
+                {/* <Col sm={4}>
+                    <img src={bitmoji1} alt="Bitmoji"  />
+                </Col> */}
+            </Row>
+            </Container>
             </div>
         </React.Fragment>
      );
