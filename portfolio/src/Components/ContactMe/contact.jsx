1495452c478b0b05acace12deef2e20cd74f7eb9
Zachary Wright <80498861+wrightzachary@users.noreply.github.com>
2022-01-04 17:54:24 -0600

    Added styling to the contact component

--- portfolio/src/Components/ContactMe/contact.jsx
+++ portfolio/src/Components/ContactMe/contact.jsx
@@ -20,26 +20,25 @@ const Contact = () => {
 
   return (
 
-    <Container>
+    <Container className='contactContainer'>
       <Row>
       {/* <Col sm={5}></Col> */}
-      <Col sm={4}>
+      <Col sm={6}>
         <div className='contact'>
         <h1>.04 Contact Me</h1>
         <hr></hr>
-        <h3>Feel free to send me an email below or </h3>
       </div>
         <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="user_name" placeholder='NAME' className="mt-2 mb-2"/>
+          <input type="text" name="user_name" placeholder='NAME' className="mt-2 mb-2 name" />
           <br></br>
-          <input type="email" name="user_email" placeholder='EMAIL' className="mt-2 mb-2"/>
+          <input type="email" name="user_email" placeholder='EMAIL' className="mt-2 mb-2 email"/>
           <br></br>
-          <textarea type="email" name="message" placeholder='MESSAGE' className="mt-2 mb-2"/>
+          <textarea type="email" name="message" placeholder='MESSAGE' className="mt-2 mb-2 message"/>
           <br></br>
-          <Button type="submit" value="Send" className="mt-2 mb-2" >Send Email</Button>
+          <Button style={{backgroundColor: "#BD871F", borderColor: "white"}} type="submit" value="Send" className="mt-2 mb-2 button" >Send Email</Button>
         </form>
         </Col>
-        <Col sm={8}></Col>
+        <Col sm={6}></Col>
       </Row>
     </Container>
    
