90e05a8506593565ee3f1b78caadea5ee363b5ea
Zachary Wright <80498861+wrightzachary@users.noreply.github.com>
2022-01-04 19:16:28 -0600

    Added styling to the projects component

--- portfolio/src/Components/Projects/projects.jsx
+++ portfolio/src/Components/Projects/projects.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Row, Card } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import igy6 from '../Images/igy6.png';
 import ecommerce from '../Images/ecommerce.png';
 import './projects.css';
@@ -9,26 +9,25 @@ import GitHub from '../Images/GitHub.PNG';
 const Projects = () => {
     return ( 
         <React.Fragment>
+            <Container className='projectsContainer'>
             <Row>
                 <Col sm={6}>
                     <div className='projectCard'>
                         <h1>.03 My Projects </h1><hr></hr>
                     </div>
                 </Col>
-                <Col sm={1}></Col>
-                <Col sm={5}></Col>
+                <Col sm={6}></Col>
             </Row>
-            <Container>
             <Row className="projectOne">
                 <div className="projectName">
-                    <h2>;IGY6<a href="https://github.com/wrightzachary/Capstone-Frontend"><FiGithub size={32} color='white'/></a></h2>
+                    <h2>;IGY6 - Veteran Suicide Awareness Application <a href="https://github.com/wrightzachary/Capstone-Frontend"><FiGithub size={32} color='#AFBDD1'/></a></h2>
                     <hr></hr>
                 </div>
                 <Col sm={6}>
                     <div className="project">
                         <p>
-                            ;IGY6 is my personal capstone comepleted in a 10 day code sprint at devCodeCamp. This application brings awareness to veteran suicide, allows users to find a VA Hospital,
-                            connect on a blog, and shows an interactive graph that displays veteran suicide rates by year. 
+                            This project was my personal capstone comepleted in a 10 day code sprint at devCodeCamp. This application was built with React.js, ASP.NET Core Web API, 
+                            and MSSQL. Using this application veterans can find a VA Hospital, connect on a blog, and shows an interactive graph that displays veteran suicide rates by year. 
                         </p>
                     </div>
                 </Col>
@@ -38,14 +37,15 @@ const Projects = () => {
                 </Row>
                 <Row className="projectTwo">
                     <div className="projectName">
-                        <h2>eCommerce<a href="https://github.com/wrightzachary/Ecommerce-Front-End"><FiGithub size={32} color='white'/></a></h2>
+                        <h2>eCommerce <a href="https://github.com/wrightzachary/Ecommerce-Front-End"><FiGithub size={32} color='#AFBDD1'/></a></h2>
                         <hr></hr>
                     </div>
                     <Col sm={6}>
                         <div className="project">
                             <p>
-                                This project was my group capstone at devCodeCamp where we made a Star Wars themed eCommerce application.
-                                This application allows users to purchase products through Stripe, post products to sell, and post and review comments for products. 
+                                This project was my group capstone at devCodeCamp where we made a Star Wars themed eCommerce application. This application was built with React.js, 
+                                ASP.NET Core Web API, and MSSQL. This application allows users to view poducts, add products to their cart,  purchase products through Stripe API,
+                                post products to sell, and post and review comments for products. 
                             </p>
                         </div>
                     </Col>
@@ -55,14 +55,15 @@ const Projects = () => {
                 </Row>
                 <Row className="projects">
                     <div className="projectName">
-                        <h2>Other Projects<a href="https://github.com/wrightzachary"><FiGithub size={32} color='white'/></a></h2>
+                        <h2>Other Projects <a href="https://github.com/wrightzachary"><FiGithub size={32} color='#AFBDD1'/></a></h2>
                         <hr></hr>
                     </div>
                     <Col sm={6}>
                         <div className="project">
                         <p>
-                            I have more projects on my GitHub. Some are console applications while some are full stack applications. Please view my GitHub to see what I have completed. I am constantly working on new projects and maintenance of older
-                            projects. Please let me know what you think!
+                            I have more projects on my GitHub that are in Python, JavaScript, C#, and Java; some projects are console applications that I did while learning the language 
+                            and some are full stack applications. Please view my GitHub to see what I have completed. I am constantly working on new projects and maintenance of older
+                            projects. 
                         </p>
                         </div>
                     </Col>
